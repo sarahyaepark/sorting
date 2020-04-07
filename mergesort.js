842da154156c3c87ec8ae09e35428fba9a755268
HagenGaryP <hagengaryp@gmail.com>
2020-04-07 15:00:13 -0400

    merge sort init

---
+++ mergesort.js
@@ -0,0 +1,17 @@
+function split(wholeArray) {
+
+  let firstHalf;
+  let secondHalf;
+
+  return [firstHalf, secondHalf];
+}
+
+function merge(arrA, arrB) {
+
+
+}
+
+function mergeSort(wholeArray) {
+
+
+}
---
+++ mergesort.spec.js
@@ -0,0 +1,17 @@
+describe('Split Array function', function() {
+  it('is able to split an array into two halves', function() {
+    // your code here
+  });
+});
+
+describe('Merge function', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    // test the merging algorithm
+  });
+});
+
+describe('mergeSort function', function(){
+  it('is able to take in a single array, split that array, sort each half then merge the sorted arrays into a single sorted array.', function(){
+    // test the merging algorithm
+  });
+});
