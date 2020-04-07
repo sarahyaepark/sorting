3b88b8f6e5a3801a40222515d09f9c7605ae10cd
HagenGaryP <hagengaryp@gmail.com>
2020-04-07 15:56:59 -0400

    recursive issue

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,32 +1,32 @@
-/* eslint-disable no-undef */
+// /* eslint-disable no-undef */
 
-describe('Bubble Sort', function(){
+// describe('Bubble Sort', function(){
 
-  it('counting number of times swap was called', function () {
-    expect(bubbleSort([1, 4, 2, 3])).toEqual([1, 2, 3, 4]);
-    spyOn(window, 'swap').and.callThrough();
-    bubbleSort([1, 4, 2, 3]);
-    expect(swap.calls.count()).toEqual(2);
-  });
+//   it('counting number of times swap was called', function () {
+//     expect(bubbleSort([1, 4, 2, 3])).toEqual([1, 2, 3, 4]);
+//     spyOn(window, 'swap').and.callThrough();
+//     bubbleSort([1, 4, 2, 3]);
+//     expect(swap.calls.count()).toEqual(2);
+//   });
 
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
-  });
+//   it('handles an empty array', function(){
+//     expect( bubbleSort([]) ).toEqual( [] );
+//   });
 
-  it('is an array', function() {
-    expect(Array.isArray(bubbleSort([]))).toEqual(true);
-  });
+//   it('is an array', function() {
+//     expect(Array.isArray(bubbleSort([]))).toEqual(true);
+//   });
 
-  it('sorts an unsorted array', function(){
-    expect( bubbleSort([3, 1, 2, 5, 7, 6, 4]) ).toEqual( [1, 2, 3, 4, 5, 6, 7] );
-  });
+//   it('sorts an unsorted array', function(){
+//     expect( bubbleSort([3, 1, 2, 5, 7, 6, 4]) ).toEqual( [1, 2, 3, 4, 5, 6, 7] );
+//   });
 
-  it('handles a already sorted array', function(){
-    expect( bubbleSort([1, 2, 3, 4, 5, 6, 7]) ).toEqual( [1, 2, 3, 4, 5, 6, 7] );
-  });
+//   it('handles a already sorted array', function(){
+//     expect( bubbleSort([1, 2, 3, 4, 5, 6, 7]) ).toEqual( [1, 2, 3, 4, 5, 6, 7] );
+//   });
 
-  it('handles an unsorted array of characters', function(){
-    expect( bubbleSort(['d', 'c', 'b', 'e', 'a']) ).toEqual( ['a', 'b', 'c', 'd', 'e'] );
-  });
-});
+//   it('handles an unsorted array of characters', function(){
+//     expect( bubbleSort(['d', 'c', 'b', 'e', 'a']) ).toEqual( ['a', 'b', 'c', 'd', 'e'] );
+//   });
+// });
 
--- mergesort.js
+++ mergesort.js
@@ -1,17 +1,55 @@
 function split(wholeArray) {
-
   let firstHalf;
   let secondHalf;
+  // console.log('whole array', wholeArray);
+
+  if (wholeArray.length < 1) {
+    return [];
+  }
+  let midpoint = Math.floor(wholeArray.length / 2);
+  firstHalf = wholeArray.slice(0, midpoint);
+  secondHalf = wholeArray.slice(midpoint);
 
   return [firstHalf, secondHalf];
 }
 
 function merge(arrA, arrB) {
+  let i = 0;
+  let j = 0;
+  let result = [];
+  while (arrA.length > i && arrB.length > j) {
+    if (arrA[i] < arrB[j]) {
+      result.push(arrA[i]);
+      i++;
+    } else {
+      result.push(arrB[j]);
+      j++;
+    }
+  }
 
+  return result.concat(arrA.slice(i)).concat(arrB.slice(j));
 
 }
 
 function mergeSort(wholeArray) {
+  console.log('whole array', wholeArray);
+  let arrSplit = split(wholeArray);
+  // console.log('>>>>', arrSplit);
+  let arrA = arrSplit[0];
+  let arrB = arrSplit[1];
+  if (arrSplit.length < 1) {
+    return wholeArray;
+  }
+
+
+
+
+  return merge(mergeSort(arrA), mergeSort(arrB));
+
+
+  // merge then call mergeSort on arrA and arrB
+
+
 
 
 }
